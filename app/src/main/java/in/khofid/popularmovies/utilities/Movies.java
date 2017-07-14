20054ec20a8c8f90d56bdb70b10fb1ff2deff35b
Khofidin <offiedz@gmail.com>
2017-07-14 19:32:14 +0700

    Implement onSaveInstanceState, so screen will be displayed the last result. And some cleanup code

--- app/src/main/java/in/khofid/popularmovies/utilities/Movies.java
+++ app/src/main/java/in/khofid/popularmovies/utilities/Movies.java
@@ -1,20 +1,23 @@
 package in.khofid.popularmovies.utilities;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by ofid on 6/20/17.
  */
 
-public class Movies {
-    int page;
-    int movie_id;
-    double vote_average;
-    String title;
-    double popularity;
-    String poster_path;
-    String original_language;
-    String original_title;
-    String overview;
-    String release_date;
+public class Movies implements Parcelable {
+    public int page;
+    public int movie_id;
+    public double vote_average;
+    public String title;
+    public double popularity;
+    public String poster_path;
+    public String original_language;
+    public String original_title;
+    public String overview;
+    public String release_date;
 
     public Movies(int page, int movie_id, double vote_average, String title, double popularity, String poster_path, String original_language, String original_title, String overview, String release_date) {
         this.page = page;
@@ -28,84 +31,47 @@ public class Movies {
         this.overview = overview;
         this.release_date = release_date;
     }
-
-    public int getPage() {
-        return page;
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
-    public void setPage(int page) {
-        this.page = page;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.page);
+        dest.writeInt(this.movie_id);
+        dest.writeDouble(this.vote_average);
+        dest.writeString(this.title);
+        dest.writeDouble(this.popularity);
+        dest.writeString(this.poster_path);
+        dest.writeString(this.original_language);
+        dest.writeString(this.original_title);
+        dest.writeString(this.overview);
+        dest.writeString(this.release_date);
     }
 
-    public int getMovieId() {
-        return movie_id;
+    protected Movies(Parcel in) {
+        this.page = in.readInt();
+        this.movie_id = in.readInt();
+        this.vote_average = in.readDouble();
+        this.title = in.readString();
+        this.popularity = in.readDouble();
+        this.poster_path = in.readString();
+        this.original_language = in.readString();
+        this.original_title = in.readString();
+        this.overview = in.readString();
+        this.release_date = in.readString();
     }
 
-    public void setMovieId(int id) {
-        this.movie_id = id;
-    }
+    public static final Parcelable.Creator<Movies> CREATOR = new Parcelable.Creator<Movies>() {
+        @Override
+        public Movies createFromParcel(Parcel source) {
+            return new Movies(source);
+        }
 
-    public double getVote_average() {
-        return vote_average;
-    }
-
-    public void setVote_average(double vote_average) {
-        this.vote_average = vote_average;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public double getPopularity() {
-        return popularity;
-    }
-
-    public void setPopularity(double popularity) {
-        this.popularity = popularity;
-    }
-
-    public String getPoster_path() {
-        return poster_path;
-    }
-
-    public void setPoster_path(String poster_path) {
-        this.poster_path = poster_path;
-    }
-
-    public String getOriginal_language() {
-        return original_language;
-    }
-
-    public void setOriginal_language(String original_language) {
-        this.original_language = original_language;
-    }
-
-    public String getOriginal_title() {
-        return original_title;
-    }
-
-    public void setOriginal_title(String original_title) {
-        this.original_title = original_title;
-    }
-
-    public String getOverview() {
-        return overview;
-    }
-
-    public void setOverview(String overview) {
-        this.overview = overview;
-    }
-
-    public String getRelease_date() {
-        return release_date;
-    }
-
-    public void setRelease_date(String release_date) {
-        this.release_date = release_date;
-    }
+        @Override
+        public Movies[] newArray(int size) {
+            return new Movies[size];
+        }
+    };
 }
